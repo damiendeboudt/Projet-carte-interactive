22a870f3b8f1539b7e813fe1d3c1e4a3ac46bbe2
Damien <damien.deboudt@sfr.fr>
2022-12-22 13:04:38 +0100

    ok with map

--- JS/script.js
+++ JS/script.js
@@ -7,17 +7,17 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 let marker = L.marker([50.23530, 4.23403]).addTo(map);
-marker.bindPopup('<img src="image/MontBlanc.png" alt="">')
+marker.bindPopup('<img src="img/Tours salamandre.png" alt="">')
 
 let circle = L.circle([50.23530, 4.23403], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.2,
+    color: 'darkorange',
+    fillColor: 'darkorange',
+    fillOpacity: 0.1,
     radius: 100
 }).addTo(map)
 
 
-    //image 2
+    //img 2
 let map2 = L.map('map2').setView([49.2123, 5.4379], 13)
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -25,11 +25,12 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map2);
 
 let marker2 = L.marker([49.21704, 5.43875]).addTo(map2);
+marker2.bindPopup('<img src="img/Douaumont.png" alt="">')
 
 let circle2 = L.circle([49.21704, 5.43875], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.2,
+    color: 'darkorange',
+    fillColor: 'darkorange',
+    fillOpacity: 0.1,
     radius: 500
 }).addTo(map2)
 
@@ -43,10 +44,11 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map3);
 
 let marker3 = L.marker([45.83266, 6.86524]).addTo(map3);
+marker3.bindPopup('<img src="img/MontBlanc.png" alt="">')
 
 let circle3 = L.circle([45.83266, 6.86524], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.2,
+    color: 'darkorange',
+    fillColor: 'darkorange',
+    fillOpacity: 0.1,
     radius: 200
 }).addTo(map3)
